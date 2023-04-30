d1b1c408ebdbc1abd889d841c48907a91fcfd700
Haoxuan Guo <kuohaoxuan@outlook.com>
2023-05-01 00:42:36 +0800

    test: add tests for subscript

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,15 @@ using Nuclides
 using Test
 
 @testset "Nuclides" begin
+    # defination and info
     C12 = Nuclide(6, 12)
     @test C12 |> atomic_number === 6
     @test C12 |> mass_number === 12
     @test C12 |> metastable_state === 0
     @test C12 |> neutron_number === 6
+
+    # subscript
+    @test "0123456789" |> super === "⁰¹²³⁴⁵⁶⁷⁸⁹"
+    @test "0123456789" |> sub === "₀₁₂₃₄₅₆₇₈₉"
+    @test "⁰¹²³⁴⁵⁶⁷⁸⁹₀₁₂₃₄₅₆₇₈₉" |> recover === "01234567890123456789"
 end
